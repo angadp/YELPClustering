7aa0e4d164566154acc3687e3d87dab1385be3e6
Angadpreet Nagpal <angadpreet.nagpal@gmail.com>
2018-10-26 11:24:50 -0700

    Added KMeans

--- Elbow.py
+++ Elbow.py
@@ -11,12 +11,20 @@ from pyspark.sql import SparkSession
 
 spark = SparkSession \
     .builder \
-    .appName("KMeans") \
-    .config("spark.some.config.option", "Angadpreet-KMeans") \
+    .appName("Elbow") \
+    .config("spark.some.config.option", "Angadpreet-Elbow") \
     .getOrCreate()
 today = dt.datetime.today()
 spark_df = spark.read.json("Data/yelp_academic_dataset_user.json").select("review_count", "average_stars", "yelping_since").rdd.map(lambda x: (x[0], x[1], (par.parse(x[2])-today).days))
 vector_df = spark_df.map(lambda s : Vectors.dense(s))
-km = KMeans()
-kme = km.train(vector_df, 3)
-print(kme.computeCost(vector_df))
+Sum_of_squared_distances = []
+K = range(1,15)
+for k in K:
+    km = KMeans()
+    kme = km.train(vector_df, k)
+    Sum_of_squared_distances.append(kme.computeCost(vector_df))
+plt.plot(K, Sum_of_squared_distances, 'bx-')
+plt.xlabel('k')
+plt.ylabel('Sum_of_squared_distances')
+plt.title('Elbow Method For Optimal k')
+plt.show()
