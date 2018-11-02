34f4fd17324d1bc6cefa9bebbf01ea0bdf883616
Angadpreet Nagpal <angadpreet.nagpal@gmail.com>
2018-11-02 12:19:40 -0700

    Adding Elbow, KMeans, Heirarchial for restaurant dataset

---
+++ Restaurants_Elbow.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+from pyspark.mllib.linalg import Vectors
+from pyspark.mllib.clustering import KMeans
+from pyspark import SparkContext, SparkConf
+import datetime as dt
+import dateutil.parser as par
+
+conf = SparkConf().setAppName("test").setMaster("local[*]")
+sc = SparkContext(conf=conf)
+from pyspark.sql import SparkSession
+
+spark = SparkSession \
+    .builder \
+    .appName("Elbow") \
+    .config("spark.some.config.option", "Angadpreet-Elbow") \
+    .getOrCreate()
+today = dt.datetime.today()
+spark_df = spark.read.json("Data/yelp_academic_dataset_business.json").select("review_count", "stars", "is_open").fillna(0).rdd.map(lambda x: (x[0], x[1], x[2]))
+vector_df = spark_df.map(lambda s : Vectors.dense(s))
+Sum_of_squared_distances = []
+K = range(1,15)
+for k in K:
+    km = KMeans()
+    kme = km.train(vector_df, k)
+    Sum_of_squared_distances.append(kme.computeCost(vector_df))
+plt.plot(K, Sum_of_squared_distances, 'bx-')
+plt.xlabel('k')
+plt.ylabel('Sum_of_squared_distances')
+plt.title('Elbow Method For Optimal k')
+plt.show()
---
+++ restaurant_KMeans.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+from pyspark.mllib.linalg import Vectors
+from pyspark.mllib.clustering import KMeans
+from pyspark import SparkContext, SparkConf
+import datetime as dt
+import dateutil.parser as par
+
+conf = SparkConf().setAppName("test").setMaster("local[*]")
+sc = SparkContext(conf=conf)
+from pyspark.sql import SparkSession
+
+spark = SparkSession \
+    .builder \
+    .appName("KMeans") \
+    .config("spark.some.config.option", "Angadpreet-KMeans") \
+    .getOrCreate()
+today = dt.datetime.today()
+spark_df = spark.read.json("Data/yelp_academic_dataset_business.json").select("review_count", "stars", "is_open").fillna(0).rdd.map(lambda x: (x[0], x[1], x[2]))
+vector_df = spark_df.map(lambda s : Vectors.dense(s))
+km = KMeans()
+kme = km.train(vector_df, 3)
+print(kme.computeCost(vector_df))
+print(kme.clusterCenters)
